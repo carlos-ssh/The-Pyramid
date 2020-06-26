1e87cb3ece48b96399c548374010e94be115545e
Carlos  Robles <carlos.anezeh@gmail.com>
2020-06-26 17:35:19 +0000

    Added instructions and an example explaining how it works this program.

---
+++ main.rb
@@ -0,0 +1,21 @@
+=begin
+This program work following these simple step:
+
+-Type a Letter and a Number on your keyboard.
+
+-And then this program will do a Pyramid with the letter that you selected multiplied for the number that you choiced.
+
+
+/////////////////
+||||EXAMPLE:||||
+\\\\\\\\\\\\\\\\\
+
+If we selected a # symbol and the number 3.
+
+This going to show the next result:
+
+                 #
+                ###
+               ##### 
+
+=end
